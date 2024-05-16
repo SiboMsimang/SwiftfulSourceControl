3332d418f45f38af77210a82c63c6c8ee8d62544
Sibo <sibomsimang@gmail.com>
2024-05-16 07:39:36 +0200

    [Clean] Add commit message documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -4,6 +4,24 @@
 //
 //  Created by Sibo Msimang on 2024/05/14.
 //
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of the patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of bug
+ 
+ MUANDANE TASKS:
+ [Clean] Description of changes
+ 
+ RELEASE
+ [Release] Description of release
+ */
 
 import SwiftUI
 
