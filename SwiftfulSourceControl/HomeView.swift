e81f89ec27e0555bf0e8c1d16e58bfc96d2ec928
Sibo <sibomsimang@gmail.com>
2024-05-16 07:24:29 +0200

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Sibo Msimang on 2024/05/16.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2")
+    }
+}
+
+#Preview {
+    HomeView()
+}
