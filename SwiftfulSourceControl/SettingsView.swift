481b428828c0c903bdc578ea4f69dee290f1dd19
Sibo <sibomsimang@gmail.com>
2024-05-22 08:21:37 +0200

    [Feature] Add SettingsView

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Sibo Msimang on 2024/05/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
