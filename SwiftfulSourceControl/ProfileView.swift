f30aa14219b27ba73f3676bafdd01c7752292115
Sibo <sibomsimang@gmail.com>
2024-05-23 08:35:07 +0200

    [Feature] Add ProfileView

---
+++ SwiftfulSourceControl/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Sibo Msimang on 2024/05/23.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Sibo!")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
